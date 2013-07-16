ae042ceaeec57035322a69cd694903f8e3737343
diegomvh <diegomvh@gmail.com>
2013-07-16 11:10:33 -0300

    Add last holder and string module for FormatString class

--- regexp/snippet.py
+++ regexp/snippet.py
@@ -3,29 +3,26 @@
 from .parser import parse_snippet
 from . import types
 
-def collect(nodes, placeholders, mirrors):
+def collect(nodes, placeholders):
     for node in nodes:
-        if isinstance(node, types.PlaceholderType):
+        if isinstance(node, (types.PlaceholderType, types.PlaceholderChoiceType)):
             if node.index not in placeholders:
                 placeholders[node.index] = node
-            mirrors.setdefault(node.index, []).append(node)
-            collect(node.content, placeholders, mirrors)
-        elif isinstance(node, types.PlaceholderChoiceType):
-            placeholders[node.index] = node
-            mirrors.setdefault(node.index, []).append(node)
-        elif isinstance(node, types.PlaceholderTransformType):
-            mirrors.setdefault(node.index, []).append(node)
+            if isinstance(node, types.PlaceholderType):
+                collect(node.content, placeholders)
 
 class Snippet(object):
     def __init__(self, source):
         self.nodes = parse_snippet(source)
         self.placeholders = {}
-        self.mirrors = {}
-        collect(self.nodes, self.placeholders, self.mirrors)
-        
+        collect(self.nodes, self.placeholders)
+        if '0' not in self.placeholders:
+            self.placeholders['0'] = types.PlaceholderType("0")
+            self.nodes.append(self.placeholders['0'])
+
     def __str__(self):
         return "".join([str(node) for node in self.nodes])
-    
+
     def replace(self, processor, memo):
         return "".join([node.replace(processor, self.placeholders, None, memo) for node in self.nodes])
 
---
+++ regexp/string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from .parser import parse_format_string
+from . import types
+
+class FormatString(object):
+    def __init__(self, source):
+        self.nodes = parse_format_string(source)
+        
+    def __str__(self):
+        return "".join([str(node) for node in self.nodes])
+    
+    def replace(self, processor, memo):
+        return "".join([node.replace(processor, {}, None, memo) for node in self.nodes])
+
+    __unicode__ = __str__
+        
